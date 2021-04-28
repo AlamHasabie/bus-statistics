8ba85c611087d8440ab0bc6d0d7878ac178cf152
Alam <alamhasabie@yahoo.com>
2021-04-29 00:41:50 +0700

    feat: refactor to use set for first occurence on a bus stop

--- mapper.py
+++ mapper.py
@@ -35,16 +35,29 @@ for line in sys.stdin:
 		# With this, we skip data with null bus stop
 		bus_stop = int(data[13])
 
-		occurence_set_val = join_values(line_id, variant, travel_number)
+		occurence_set_val = join_values(line_id, variant, travel_number, bus_stop)
+
+		# Data is already sorted by timestamp
+		# Original paper uses control point, so we want to simulate bus stops as control points
+		# To do this, we just take the first occurence of data with key :
+		# line_id, variant, travel_number, bus_stop
+
+		# bus_stop means where the bus starts from
+		# so , transition from bus stop 1 -> bus stop 2 means that the bus arrives at bus stop 2
+		# so we take the first occurence of said data to be
+
+		# Additionally, we may have some bus stops without at stops = 1
+		# This is fine, since we can assume that :
+		# 1. We can predict the data from the first timestamp, or
+		# 2. The bus did not stop at said bus stop
 
 		# Format : line_id.variant.travel_number.timestamp
 		# Key : compound line_id.variant(primary).travel_number.timestamp(secondary, sorting)
 		# info : travel_number.timestamp.bus_stop.timeframe
-		at_stop = data[14]
-		if data[14] != "0" and (occurence_set_val not in occurence_set):
+		if occurence_set_val not in occurence_set :
 			occurence_set.add(occurence_set_val)
 			key = join_values(line_id, variant, travel_number, timestamp)
-			value = join_values(travel_number, timestamp, bus_stop, timeframe, at_stop)
+			value = join_values(travel_number, timestamp, bus_stop, timeframe)
 			print("{}\t{}".format(key, value))
 
 	except Exception :
--- reducer.py
+++ reducer.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python
 import sys
 
-current_count = 0
-current_key = None
-
+current_travel_number = None
+current_start_timestamp = None
 for line in sys.stdin:
 
-	data = line.strip().split("\t")
-	print(data)
+	key, value = line.strip().split("\t")
+
+	line_id , variant, _, _ = key.split(".")
+	travel_number, timestamp, bus_stop, timeframe = value.split(".")
+
+	timestamp = int(timestamp)
+
+	if current_travel_number is None or current_travel_number != travel_number :
+		current_travel_number = travel_number
+		current_start_timestamp = timestamp
+	else :
+		time_difference = timestamp - current_start_timestamp
+		print("{}.{}.{}.{}\t{}").format(line_id, variant, timeframe, bus_stop, time_difference)
